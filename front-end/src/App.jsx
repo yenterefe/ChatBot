c2c99693fe73aab532d4a7bb0e88617be4044933
yenterefe <yenterefe@gmail.com>
2024-09-22 22:36:39 -0400

    added chat style question and answer for the frontend

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,6 +6,10 @@ import './App.css'
 function App() {
   const [userInput, setUserInput] = useState("");
   const [answer, setAnswer] = useState("");
+  //keeping these for future reference
+  // const [questions, setQuestions] = useState([]);
+  // const [botAnswers, setBotAnswers] = useState([]);
+  const [qas, setQas] = useState([]);
 
   async function handleSubmission() {
     try {
@@ -13,6 +17,15 @@ function App() {
       setUserInput('');
       console.log(response.data);
       setAnswer(response.data.answer);
+      // setQuestions([...questions, userInput]);
+      // setBotAnswers([...botAnswers, response.data.answer]);
+      setQas([...qas,
+      // created an object to store the two information, question and answer so when I map them they will be paired together!! 
+      {
+        question: userInput,
+        answer: response.data.answer
+      }
+      ])
     } catch (error) {
       console.error("Error submitting question:", error);
     }
@@ -26,7 +39,24 @@ function App() {
     <>
       <input type="text" placeholder='Ask about me' value={userInput} onChange={handleInput} />
       <button onClick={handleSubmission}> Submit</button>
-      <p>{answer}</p>
+      {/* also keeping for future reference */}
+      {/* <ul>
+        {questions.map((question, index) => (
+          <li key={index}>Question: {question}</li>
+        ))}
+        {botAnswers.map((botAnswer, index) => (
+          <li key={index}>Answer:{botAnswer}</li>
+        ))}
+      </ul> */}
+      <ul>
+        {/* index is always second place while using the map method */}
+        {qas.map((information, index) => (
+          <li key={index}>
+            Question: {information.question}<br /> Answer: {information.answer}
+          </li>
+        ))}
+      </ul>
+
     </>
   )
 }
